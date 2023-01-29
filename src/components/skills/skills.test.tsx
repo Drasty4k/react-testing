d07aea0410ccbe103afe87e9bcaffb28c7a7f5c2
Dragos Barbuta <barbuta.dragos12@gmail.com>
2023-01-29 14:21:15 +0100

    used the findByRole method to await a promise by a specified timeout

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -27,8 +27,22 @@ describe("Skills", () => {
   test("Start Learning button is not rendered", () => {
     render(<Skills skills={skills} />);
     const startLearningButton = screen.queryByRole("button", {
-      name: "Start learning"
-    })
-    expect(startLearningButton).not.toBeInTheDocument()
-  })
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
+
+  test("Start Learning button is eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start learning",
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
