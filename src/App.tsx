80917b807a084fa0e18a699148519a78b5cc2f5b
Dragos Barbuta <barbuta.dragos12@gmail.com>
2023-01-29 16:13:07 +0100

    keyboard interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Application } from './components/application/application';
+import { Counter } from './components/counter/counter';
 import { Skills } from './components/skills/skills';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Application />
       <Skills skills={["HTML", "CSS"]} />
+      <Counter />
     </div>
   );
 }
